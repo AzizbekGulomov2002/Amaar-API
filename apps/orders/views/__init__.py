1658386e5903367e192eb855052b738e05580e71
adkhamjon <adkhamjonmakmudov@gmail.com>
2024-08-04 20:30:15 +0500

    fixed project and payment

---
+++ apps/orders/views/__init__.py
@@ -0,0 +1,7 @@
+from .base_views import *
+from .infos_views import *
+from .order_views import *
+from .payment_views import *
+from .product_views import *
+from .user_views import *
+from .webhook import *
