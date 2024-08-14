cdde73b657f04b351626d37a2be0472bd98584f4
Azizbek G`ulomov <84550212+AzizbekGulomov2002@users.noreply.github.com>
2024-08-14 11:31:13 +0500

    update category

--- apps/orders/models/products.py
+++ apps/orders/models/products.py
@@ -24,7 +24,7 @@ class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name_uz or self.name_ru or self.name_en
+        return self.name_ru or self.name_en
 
     class Meta:
         ordering = ['name_uz', 'name_ru', 'name_en']
