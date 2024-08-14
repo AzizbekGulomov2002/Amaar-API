523b409cf6c6d69938bc3a1406be7f1efcce61b5
Azizbek G`ulomov <84550212+AzizbekGulomov2002@users.noreply.github.com>
2024-08-14 11:29:08 +0500

    --update filter

--- apps/orders/filters/product_filters.py
+++ apps/orders/filters/product_filters.py
@@ -6,7 +6,7 @@ from apps.orders.models import Category, Product
 class CategoryFilter(django_filters.FilterSet):
     class Meta:
         model = Category
-        fields = ['name_uz', 'name_ru', 'name_en']
+        fields = ['name_ru', 'name_en']
 
 
 class ProductFilter(django_filters.FilterSet):
