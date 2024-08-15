89e295d4affcbf7086d6b44f31387902010fee87
adkhamjon <adkhamjonmakmudov@gmail.com>
2024-08-15 17:16:29 +0500

    last V1 updated

--- config/urls.py
+++ config/urls.py
@@ -4,21 +4,24 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
         title="Tashkent Market API",
         default_version='v1',
-        description="API documentation for Tashkent market API",
-        contact=openapi.Contact(email="contact@example.com"),
+        description="API documentation for Tashkent Market",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@tashkentmarket.ae"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
+    permission_classes=(permissions.AllowAny,),  # noqa
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('apps.urls')),
+    path('api/', include('apps.urls')),  # Include your application URLs
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
