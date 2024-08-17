fe156cc0a58a1c2745e9645a6db9294e535337d7
adkhamjon <adkhamjonmakmudov@gmail.com>
2024-08-17 15:21:31 +0500

    fixed the project send_tg

---
+++ apps/orders/tasks.py
@@ -0,0 +1,24 @@
+import asyncio
+
+from celery import shared_task
+
+from apps.orders.models.orders import Order
+from apps.orders.views.tg import send_order_to_telegram_async
+
+
+@shared_task
+def send_order_to_telegram_task(order_id):
+    try:
+        order = Order.objects.get(id=order_id)
+        try:
+            loop = asyncio.get_event_loop()
+            if loop.is_closed():
+                loop = asyncio.new_event_loop()
+                asyncio.set_event_loop(loop)
+        except RuntimeError:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+        loop.run_until_complete(send_order_to_telegram_async(order, created=True))
+
+    except Exception as e:
+        raise Exception(f"Error sending order to Telegram: {e}")
