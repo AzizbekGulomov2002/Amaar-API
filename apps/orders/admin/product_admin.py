343d66a4a947ce07b178e7322bc568604fca25ac
AzizbekGulomov2002 <azizgulomov1529@gmail.com>
2024-10-12 10:38:23 +0500

    [UPDATED] Product admin

--- apps/orders/admin/product_admin.py
+++ apps/orders/admin/product_admin.py
@@ -5,12 +5,25 @@ from django.utils.html import format_html
 from apps.orders.models.products import Product, ProductImage, Category
 
 
+# Define an inline admin for ProductImage
 class ProductImageInline(admin.TabularInline):
     model = ProductImage
     extra = 1
     can_delete = True
     show_change_link = True
 
+
+# Define the admin for the Product model
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name_uz', 'name_ru', 'name_en', 'price', 'quantity', 'category', 'best_deals', 'show_main_page', 'created_at')
+    list_filter = ('category', 'best_deals', 'show_main_page')
+    search_fields = ('name_uz', 'name_ru', 'name_en', 'price')
+    ordering = ['name_uz']
+    readonly_fields = ('created_at',)
+    inlines = [ProductImageInline]
+
+# Register the Product model with the admin site
+admin.site.register(Product, ProductAdmin)
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id','name_uz', 'name_ru', 'name_en', 'created_at')
@@ -21,12 +34,3 @@ admin.site.register(Category, CategoryAdmin)
 
 
 
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id','name_uz', 'name_ru', 'name_en', 'price', 'quantity', 'category', 'best_deals', 'show_main_page', 'created_at')
-    list_filter = ('category', 'best_deals', 'show_main_page')
-    search_fields = ('name_uz', 'name_ru', 'name_en', 'price')
-    ordering = ['name_uz', 'name_ru', 'name_en']
-    readonly_fields = ('created_at',)
-
-# Register the models with the admin site
-admin.site.register(Product, ProductAdmin)
